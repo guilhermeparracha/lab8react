2f8f7edb080172f1ea752d32d853b51938e9e086
Guilherme-Parracha-a22402027 <a22402027@alunos.ulht.pt>
2025-11-20 09:13:49 +0000

    lab8 guardado

---
+++ app/características/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function page() {
+    const caracteristicas = [
+        'JSX, sintaxe que mistura HTML e JS.',
+        'Componentes, funções que retornam JSX.',
+        'Componentes Reutilizáveis e Modulares.',
+        'Roteamento Automático e APIs.',
+        'Hooks: useState, useEffect e useSWR.',
+        'Renderização Rápida e SEO Friendly.',
+        'TypeScript Seguro e Escalável.',
+        'Comunidade Ativa e Popularidade.'
+    ]
+  return (
+    <div>page</div>
+  )
+}
